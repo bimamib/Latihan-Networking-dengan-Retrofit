6ca207baa2fa5eae89d184dd1e7a72b1d16b5aed
bimamib <bimaprast1601@gmail.com>
2023-09-11 19:17:48 +0700

    membuat package baru (data and ui)

--- app/src/main/java/com/bima/restaurantreview/ui/MainActivity.kt
+++ app/src/main/java/com/bima/restaurantreview/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bima.restaurantreview
+package com.bima.restaurantreview.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bima.restaurantreview.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
