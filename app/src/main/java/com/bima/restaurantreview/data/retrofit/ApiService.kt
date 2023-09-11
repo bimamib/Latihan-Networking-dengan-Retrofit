f1ed1a4e6f068ae0431ce5c408838b6451b9cfe0
bimamib <bimaprast1601@gmail.com>
2023-09-11 19:43:54 +0700

    add code ApiService

--- app/src/main/java/com/bima/restaurantreview/data/retrofit/ApiService.kt
+++ app/src/main/java/com/bima/restaurantreview/data/retrofit/ApiService.kt
@@ -1,4 +1,13 @@
 package com.bima.restaurantreview.data.retrofit
 
-class ApiService {
+import com.bima.restaurantreview.data.response.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
 }
